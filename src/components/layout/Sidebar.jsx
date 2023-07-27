a4abda3e384ebba257c265f45dd7b3a0618b7c51
HaroldObasi <haroldobasi2k16@gmail.com>
2023-07-27 19:14:58 +0100

    added upload button to sidebar

--- src/components/layout/Sidebar.jsx
+++ src/components/layout/Sidebar.jsx
@@ -1,21 +1,49 @@
-import React from "react";
+import React, { useRef } from "react";
 import { useGlobalContext } from "@/contexts/AppContext";
 
 const Sidebar = () => {
-  const { setFileName } = useGlobalContext();
+  const { fileName, setFileName, setFileContent } = useGlobalContext();
+  const fileInputRef = useRef(null);
+
+  function readFile(file) {
+    const reader = new FileReader();
+    reader.onload = (evt) => {
+      const linesArray = reader.result.split("\n");
+      setFileContent(linesArray);
+    };
+    if (file) {
+      reader.readAsText(file);
+    }
+  }
 
   const onClickUpload = () => {
-    setFileName("The life and times");
+    fileInputRef.current.click();
+  };
+
+  const handleFileChange = () => {
+    const file = fileInputRef.current.files[0];
+    setFileName(file.name);
+    readFile(file);
   };
 
   return (
-    <div class="bg-slate-400 w-60 h-full flex-none">
-      <div className="flex justify-center py-8">
+    <div class="bg-slate-400 w-[300px] h-full flex-none">
+      <div className="flex justify-center py-8 px-3">
         <button
           onClick={onClickUpload}
           className="py-2 px-3 text-gray-900 rounded-md border-2 border-dashed hover:border-blue-500 hover:bg-blue-50"
         >
-          Click here to upload
+          {fileName === "" ? (
+            <p>Click to upload a .txt file here</p>
+          ) : (
+            <p>Uploaded: {fileName}, Click to change file </p>
+          )}
+          <input
+            className="input-field hidden"
+            type="file"
+            ref={fileInputRef}
+            onChange={handleFileChange}
+          />
         </button>
       </div>
     </div>
