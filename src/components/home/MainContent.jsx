245ea7c8963ecb13028ab63a31d424073fcc50b4
HaroldObasi <haroldobasi2k16@gmail.com>
2023-07-28 11:52:53 +0100

    refactor

--- src/components/home/MainContent.jsx
+++ src/components/home/MainContent.jsx
@@ -1,17 +1,18 @@
 import React, { useEffect, useState } from "react";
-import { extractGroupMembers } from "@/utils/countUserTokens";
+import {
+  extractGroupMembers,
+  extractMessageCount,
+} from "@/utils/countUserTokens";
 import { useGlobalContext } from "@/contexts/AppContext";
 import ChartComponent from "../ChartComponent";
 
 const MainContent = () => {
   const { fileName, fileContent } = useGlobalContext();
-  //   const [groupData, setGroupData] = useState({});
   const [groupMembers, setGroupMembers] = useState([]);
   const [groupTokenCount, setGroupTokenCount] = useState([]);
 
   useEffect(() => {
     if (fileContent.length > 1) {
-      //   setGroupData(extractGroupMembers(fileContent));
       const groupData = extractGroupMembers(fileContent);
       const entries = Object.entries(groupData);
       setGroupMembers(entries.map((item) => item[0]));
@@ -20,14 +21,16 @@ const MainContent = () => {
   }, [fileContent]);
 
   return (
-    <div className="px-3 py-2">
+    <div className="px-3 py-2 max-h-screen">
       <p className=" text-2xl">
         Your Whatsapp group chat stats will be shown here
       </p>
 
-      {groupMembers ? (
-        <div className="bg-sky-100 overflow-y-auto max-h-[400px]">
-          <p>These are all {groupMembers.length} your group members</p>
+      {groupMembers.length > 0 ? (
+        <div className="bg-sky-100 overflow-y-auto max-h-[200px] px-3 rounded-sm">
+          <p className="my-1 sticky">
+            These are all {groupMembers.length} your group members
+          </p>
           {groupMembers.map((item) => (
             <p key={item}>{item}</p>
           ))}
--- src/pages/index.jsx
+++ src/pages/index.jsx
@@ -1,87 +1,11 @@
-import { useRef, useState } from "react";
-import { extractGroupMembers } from "@/utils/countUserTokens";
-import { useGlobalContext } from "@/contexts/AppContext";
-
-import ChartComponent from "@/components/ChartComponent";
 import Sidebar from "@/components/layout/Sidebar";
+import MainContent from "@/components/home/MainContent";
 
 export default function Home() {
-  const { fileName } = useGlobalContext();
-  // const fileInputRef = useRef(null);
-  // const [fileContent, setFileContent] = useState([]);
-  // const [fileName, setFileName] = useState("");
-  // const [groupMembers, setGroupMembers] = useState({});
-
-  function readFile(file) {
-    const reader = new FileReader();
-    reader.onload = (evt) => {
-      const linesArray = reader.result.split("\n");
-      setFileContent(linesArray);
-    };
-    if (file) {
-      reader.readAsText(file);
-    }
-  }
-
-  const handleClick = () => {
-    setGroupMembers(extractGroupMembers(fileContent));
-  };
-
-  const handleFileChange = () => {
-    const file = fileInputRef.current.files[0];
-    setFileName(file.name);
-    readFile(file);
-  };
-
-  // return (
-  //   <>
-  //     <Sidebar />
-  //     <div className="py-5 px-3">
-  //       <h1 className="font-sans font-bold text-center">
-  //         Upload your Whatsapp txt file here
-  //       </h1>
-  //       <ChartComponent data={Object.entries(groupMembers)} />
-  //       <div className="flex justify-center py-8">
-  //         <button
-  //           onClick={() => {
-  //             fileInputRef.current.click();
-  //           }}
-  //           className="border border-dashed rounded-md border-blue-300 hover:border-blue-800 w-[400px] h-[300px]"
-  //         >
-  //           {fileName === "" ? (
-  //             <p>Click to upload a .txt file here</p>
-  //           ) : (
-  //             <p>Uploaded: {fileName}, Click to change file </p>
-  //           )}
-  //           <input
-  //             className="input-field hidden"
-  //             type="file"
-  //             ref={fileInputRef}
-  //             onChange={handleFileChange}
-  //           />
-  //         </button>
-  //       </div>
-
-  //       {fileContent.length > 0 ? (
-  //         <div className="flex justify-center">
-  //           <button
-  //             className="px-2 py-1 bg-slate-500 hover:bg-slate-600 text-white"
-  //             onClick={handleClick}
-  //           >
-  //             Generate
-  //           </button>
-  //         </div>
-  //       ) : (
-  //         <></>
-  //       )}
-  //     </div>
-  //   </>
-  // );
-
   return (
-    <div class="flex h-screen">
+    <div className="flex h-screen max-h-screen">
       <Sidebar />
-      <div class="bg-sky-400 flex-grow">This is the file name: {fileName} </div>
+      <MainContent />
     </div>
   );
 }
