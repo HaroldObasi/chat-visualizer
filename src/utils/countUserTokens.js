95a36c90731c815e6b54a8e1fea2e52cec0ba0d6
HaroldObasi <haroldobasi2k16@gmail.com>
2023-07-28 11:52:34 +0100

    function to find message count by a user

--- src/utils/countUserTokens.js
+++ src/utils/countUserTokens.js
@@ -23,3 +23,23 @@ export const extractGroupMembers = (data, groupMembers = {}) => {
   delete groupMembers[groupName];
   return groupMembers;
 };
+
+export const extractMessageCount = (data, messageCount = {}) => {
+  const regex = /\[\d{2}\/\d{2}\/\d{4}, \d{2}:\d{2}:\d{2}\] ([^:]+):/;
+
+  const groupName = data[0].match(regex)[1];
+
+  for (let line of data) {
+    const matchResult = line.match(regex);
+    if (matchResult && matchResult.length > 1) {
+      const name = matchResult[1].trim();
+      if (groupMembers[name] === undefined) {
+        groupMembers[name] = 1;
+      } else {
+        groupMembers[name]++;
+      }
+    }
+  }
+
+  return groupMembers;
+};
