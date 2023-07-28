311d08683ab8561681c7233526aa29190f41f774
HaroldObasi <haroldobasi2k16@gmail.com>
2023-07-28 11:51:20 +0100

    changed chat import, added some options

--- src/components/ChartComponent.jsx
+++ src/components/ChartComponent.jsx
@@ -1,23 +1,22 @@
-import React, { useState, useEffect } from "react";
-import {
-  Chart as ChartJS,
-  BarElement,
-  CategoryScale,
-  LinearScale,
-  Tooltip,
-  Legend,
-} from "chart.js";
-import { Bar } from "react-chartjs-2";
-
-ChartJS.register(BarElement, CategoryScale, LinearScale, Tooltip, Legend);
+import React from "react";
+// import {
+//   Chart as ChartJS,
+//   BarElement,
+//   LineElement,
+//   CategoryScale,
+//   LinearScale,
+//   Tooltip,
+//   Legend,
+// } from "chart.js";
+import "chart.js/auto";
+import { Bar, Line } from "react-chartjs-2";
 
 const ChartComponent = ({ labels, values }) => {
-  const [entries, setEntries] = useState([]);
   const data = {
     labels: labels,
     datasets: [
       {
-        labels: "369",
+        label: "Who's sent the most '✅' ",
         data: values,
         backgroundColor: "aqua",
         borderColor: "black",
@@ -26,11 +25,43 @@ const ChartComponent = ({ labels, values }) => {
     ],
   };
 
-  const options = {};
+  const options = {
+    plugins: {
+      subtitle: {
+        display: true,
+        text: "Custom Chart Subtitle",
+      },
+      title: {
+        display: true,
+        text: "Custom Chart Title",
+      },
+    },
+    scales: {
+      y: {
+        // defining min and max so hiding the dataset does not change scale range
+        min: 0,
+        max: Math.max(...values) + 3,
+      },
+    },
+    elements: {
+      point: {
+        radius: 3,
+        hoverRadius: 5,
+      },
+      line: {
+        tension: 0,
+        backgroundColor: "aqua",
+        borderWidth: 5,
+        borderColor: "red",
+        fill: true,
+      },
+    },
+    responsive: true,
+  };
 
   return (
-    <div>
-      <Bar data={data} options={options}></Bar>
+    <div className="relative h-[40vh] w-[80%]">
+      <Line data={data} options={options}></Line>
     </div>
   );
 };
