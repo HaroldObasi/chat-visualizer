ebfa979bdbad8a8a0dbcc842215af9dd8ec6d6a3
HaroldObasi <haroldobasi2k16@gmail.com>
2023-08-13 02:28:38 +0100

    added max width

--- src/components/home/MainContent.jsx
+++ src/components/home/MainContent.jsx
@@ -20,35 +20,37 @@ const MainContent = () => {
 
   return (
     <div className="flex-1 h-screen overflow-y-auto bg-zinc-800">
-      <div className="p-3 bg-zinc-800 sticky top-0 z-10 border-b border-zinc-500">
-        <div className="relative">
-          <HiOutlineSearch className="text-xl text-zinc-600 absolute top-0 bottom-0 my-auto left-1" />
-          <input
-            className="pr-1 pl-9 py-1 my-1 rounded-sm bg-zinc-500 w-full"
-            type="text"
-            onChange={(e) => setSearchToken(e.target.value)}
-          />
-        </div>
-      </div>
-
-      <div className="px-3">
-        <p className="my-3 text-2xl">
-          Your Whatsapp group chat stats will be shown here
-        </p>
-
-        {groupMembers.length > 0 ? (
-          <div className="bg-zinc-700 overflow-y-auto max-h-[200px] px-3 rounded-sm ">
-            <p className="my-1 sticky top-0 bg-zinc-700 text-xl">
-              These are all {groupMembers.length} of your group members
-            </p>
-            {groupMembers.map((item) => (
-              <p key={item}>{item}</p>
-            ))}
+      <div className="max-w-screen-xl mx-auto">
+        <div className="p-3 bg-zinc-800 sticky top-0 z-10 border-b border-zinc-500 ">
+          <div className="relative">
+            <HiOutlineSearch className="text-xl text-zinc-600 absolute top-0 bottom-0 my-auto left-1" />
+            <input
+              className="pr-1 pl-9 py-1 my-1 rounded-sm bg-zinc-500 w-full"
+              type="text"
+              onChange={(e) => setSearchToken(e.target.value)}
+            />
           </div>
-        ) : (
-          <></>
-        )}
-        <ChartComponent labels={groupMembers} values={groupTokenCount} />
+        </div>
+
+        <div className="px-3">
+          <p className="my-3 text-2xl">
+            Your Whatsapp group chat stats will be shown here
+          </p>
+
+          {groupMembers.length > 0 ? (
+            <div className="bg-zinc-700 overflow-y-auto max-h-[200px] px-3 rounded-sm ">
+              <p className="my-1 sticky top-0 bg-zinc-700 text-xl">
+                These are all {groupMembers.length} of your group members
+              </p>
+              {groupMembers.map((item) => (
+                <p key={item}>{item}</p>
+              ))}
+            </div>
+          ) : (
+            <></>
+          )}
+          <ChartComponent labels={groupMembers} values={groupTokenCount} />
+        </div>
       </div>
     </div>
   );
