f01a05c1bda6a3a5a0a0beec4b1c00eec36386ab
HaroldObasi <haroldobasi2k16@gmail.com>
2023-08-13 02:23:16 +0100

    window resize handler

--- src/pages/index.jsx
+++ src/pages/index.jsx
@@ -1,12 +1,30 @@
 import { useState, useEffect } from "react";
 import Sidebar from "@/components/layout/Sidebar";
+import { useGlobalContext } from "@/contexts/AppContext";
+import MainContent from "@/components/home/MainContent";
 
 export default function Home() {
-  const [sidebarOpen, setSidebarOpen] = useState(false);
+  useEffect(() => {
+    const handleResize = () => {
+      if (window.innerWidth < 600) {
+        setSidebarOpen(false);
+      } else {
+        setSidebarOpen(true);
+      }
+    };
+    handleResize();
+
+    window.addEventListener("resize", handleResize);
+
+    return () => {
+      window.removeEventListener("resize", handleResize);
+    };
+  }, []);
+  const { setSidebarOpen } = useGlobalContext();
   return (
-    <div className="flex">
+    <div className="flex overflow-y-hidden">
       <Sidebar />
-      <div className="h-screen flex-1 px-5 py-2">main content</div>
+      <MainContent />
     </div>
   );
 }
